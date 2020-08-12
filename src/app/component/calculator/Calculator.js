669c39571809659f3d76c131c4b335d42516d663
zkj <438101845@qq.com>
2020-08-13 00:38:54 +0800

    [zkj]-{feat}-: add Calculator.js

--- src/app/component/calculator/Calculator.js
+++ src/app/component/calculator/Calculator.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
 
 class Calculator extends React.Component {
   render() {
-    return <h1>hello</h1>
+    return <div>
+      <h3>在线计算器</h3>
+      <div className="calculator-dis">
+        <span className="display">555</span>
+        <button className="seven"></button>
+        <button className="eight"></button>
+        <button className="nine"></button>
+        <button className="four"></button>
+        <button className="five"></button>
+        <button className="six"></button>
+        <button className="one"></button>
+        <button className="two"></button>
+        <button className="three"></button>
+        <button className="zero"></button>
+        <button className="clear"></button>
+        <button className="equal"></button>
+      </div>
+      <Link to="/">回到主页</Link>
+    </div>
   }
 };
 
