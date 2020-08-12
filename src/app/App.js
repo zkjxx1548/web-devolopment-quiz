3e1ceb181c146db339e50eb581123e5c8d605332
zkj <438101845@qq.com>
2020-08-12 23:43:09 +0800

    [zkj]-{feat}-: add header.css

--- src/app/App.js
+++ src/app/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="app">
         <BrowserRouter>
-          <div>
+          <div className="header">
             <Link to="/">HOME</Link>
             <Link to="/calculator">在线计算器</Link>
             <Link to="/timer">在线倒计时器</Link>
