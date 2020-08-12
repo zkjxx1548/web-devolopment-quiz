b1157089f4e4d16a7fb5f8af469bbf7818cd6e2f
zkj <438101845@qq.com>
2020-08-13 00:21:20 +0800

    [zkj]-{feat}-: modify span to Link

--- src/app/component/home/Home.js
+++ src/app/component/home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './home.less';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (<div className="home">
@@ -7,11 +8,11 @@ const Home = () => {
     <div className="tools">
       <div className="tool">
         <img src={require("../../../images/calculator.png")} alt="not internet"/>
-        <span>计算器</span>
+        <Link to="/calculator">计算器</Link>
       </div>
       <div className="tool">
         <img src={require("../../../images/timer.png")} alt="not internet"/>
-        <span>倒计时器</span>
+        <Link to="/timer">倒计时器</Link>
       </div>
     </div>
   </div>);
