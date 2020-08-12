5f205849825c19c62ebbaf94f040505158b7f044
zkj <438101845@qq.com>
2020-08-12 23:49:51 +0800

    [zkj]-{feat}-: add Home.js

--- src/app/component/home/Home.js
+++ src/app/component/home/Home.js
@@ -3,7 +3,17 @@ import './home.less';
 
 const Home = () => {
   return (<div className="home">
-    This is home page
+    <span>在线实用工具</span>
+    <div className="tools">
+      <div className="tool">
+        <img src={require("../../../images/calculator.png")} alt="not internet"/>
+        <span>计算器</span>
+      </div>
+      <div className="tool">
+        <img src={require("../../../images/timer.png")} alt="not internet"/>
+        <span>倒计时器</span>
+      </div>
+    </div>
   </div>);
 };
 
