26e1a800346d2fad83bdd321e0faac37115a6ad4
zkj <438101845@qq.com>
2020-08-13 01:12:54 +0800

    [zkj]-{feat}-: add Calculator.css

--- src/app/component/calculator/Calculator.js
+++ src/app/component/calculator/Calculator.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import './calculator.css';
 
 
 class Calculator extends React.Component {
   render() {
     return <div>
-      <h3>在线计算器</h3>
+      <h3 className="calculator-title">在线计算器</h3>
       <div className="calculator-dis">
         <span className="display">555</span>
-        <button className="seven"></button>
-        <button className="eight"></button>
-        <button className="nine"></button>
-        <button className="four"></button>
-        <button className="five"></button>
-        <button className="six"></button>
-        <button className="one"></button>
-        <button className="two"></button>
-        <button className="three"></button>
-        <button className="zero"></button>
-        <button className="clear"></button>
-        <button className="equal"></button>
+        <button className="increament yellow btn1">+</button>
+        <button className="decreament yellow btn1">-</button>
+        <button className="multiple yellow btn1">x</button>
+        <button className="seven orange btn">7</button>
+        <button className="eight orange btn">8</button>
+        <button className="nine orange btn">9</button>
+        <button className="four orange btn">4</button>
+        <button className="five orange btn">5</button>
+        <button className="six orange btn">6</button>
+        <button className="one orange btn">1</button>
+        <button className="two orange btn">2</button>
+        <button className="three orange btn">3</button>
+        <button className="zero orange btn">0</button>
+        <button className="clear btn">Clear</button>
+        <button className="equal btn">=</button>
       </div>
       <Link to="/">回到主页</Link>
     </div>
