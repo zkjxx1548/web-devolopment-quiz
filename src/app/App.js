e736905e13031ba1a6d57cfd70c3bbf460c80ded
zkj <438101845@qq.com>
2020-08-12 23:30:40 +0800

    [zkj]-{feat}-: init Route

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 import './app.less';
-import {Route, BrowserRouter} from "react-router-dom";
-import Home from "./Home";
+import {Route, BrowserRouter, Link, Switch} from "react-router-dom";
+import Home from "./component/home/Home";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <BrowserRouter>
-          <Route exact path='/' component={Home}/>
+          <div>
+            <Link to="/">HOME</Link>
+            <Link to="/calculator">在线计算器</Link>
+            <Link to="/timer">在线倒计时器</Link>
+          </div>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/calculator' component={Calculator}/>
+            <Route path='/timer' component={Timer}/>
+          </Switch>
         </BrowserRouter>
       </div>
     );
