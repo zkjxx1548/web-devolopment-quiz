11da29acc0a040024cb47aab7ab0b0ad4f40feb4
zkj <438101845@qq.com>
2020-08-14 14:57:33 +0800

    add Header.css

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, NavLink, Route } from 'react-router-dom';
 import Store from '../component/home/Store';
+import './header.css';
 
 class Header extends React.Component {
   render() {
     return <div>
       <BrowserRouter>
-        <ul>
+        <ul className="nav">
           <li>
             <NavLink to="/">商城</NavLink>
+          </li>
+          <li>
+            <NavLink to="/order">订单</NavLink>
+          </li>
+          <li>
+            <NavLink to="/add">添加商品</NavLink>
           </li>
         </ul>
         <Switch>
