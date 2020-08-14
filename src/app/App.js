bb119963dceaba597da90c3e83eb037dc8b93076
zkj <438101845@qq.com>
2020-08-14 13:48:13 +0800

    init

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,11 @@
 import React, {Component} from 'react';
 import './app.less';
-import {Route, BrowserRouter, Link, Switch} from "react-router-dom";
-import Home from "./component/home/Home";
-import Calculator from './component/calculator/Calculator';
-import Timer from './component/timer/Timer'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <BrowserRouter>
-          <div className="header">
-            <Link to="/">HOME</Link>
-            <Link to="/calculator">在线计算器</Link>
-            <Link to="/timer">在线倒计时器</Link>
-          </div>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/calculator' component={Calculator}/>
-            <Route path='/timer' component={Timer}/>
-          </Switch>
-        </BrowserRouter>
+        hello
       </div>
     );
   }
