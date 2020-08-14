dd9dbe9ac287215174e74da3b49d5561ad4d4efe
zkj <438101845@qq.com>
2020-08-14 15:54:09 +0800

    modify Store.js

--- src/app/component/home/Product.js
+++ src/app/component/home/Product.js
@@ -6,8 +6,8 @@ class Product extends React.Component {
   render() {
     return <div className="product">
       <img src={require("../../../images/1.png")} alt="sorry"/>
-      <span>可乐1</span>
-      <span>单价：1元/瓶</span>
+      <span>{this.props.product.name}</span>
+      <span>单价：{this.props.product.price}元/瓶</span>
       <button>+</button>
     </div>
   }
--- src/app/component/home/Store.js
+++ src/app/component/home/Store.js
@@ -3,14 +3,44 @@ import Product from './Product';
 import './store.css';
 
 class Store extends React.Component {
+  state = {
+    products: [
+      {
+        img_url: "../../../images/1.png",
+        name: "可乐1",
+        price: 1
+      },
+      {
+        img_url: "../../../images/1.png",
+        name: "可乐2",
+        price: 1
+      },
+      {
+        img_url: "../../../images/1.png",
+        name: "可乐3",
+        price: 1
+      },
+      {
+        img_url: "../../../images/1.png",
+        name: "可乐4",
+        price: 1
+      },
+      {
+        img_url: "../../../images/1.png",
+        name: "可乐5",
+        price: 1
+      },
+      {
+        img_url: "../../../images/1.png",
+        name: "可乐6",
+        price: 1
+      }
+    ]
+  }
+
   render() {
     return <div className="products">
-      <Product /> 
-      <Product /> 
-      <Product /> 
-      <Product />    
-      <Product /> 
-      <Product /> 
+      {this.state.products.map(product => <Product product={product} />)} 
     </div>
   }
 }
